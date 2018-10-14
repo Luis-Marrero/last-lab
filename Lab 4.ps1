56810f32cc1062e3e82b686d7832a38610ac96a7
Luis-Marrero <44122162+Luis-Marrero@users.noreply.github.com>
2018-10-14 15:47:32 -0500

    Add files via upload
    Added Unit 4 lab

---
+++ Lab 4.ps1
@@ -0,0 +1,7 @@
+﻿Set-location C:\
+New-item -type directory -name fileserver
+$Names = import-csv employeelist.csv 
+ForEach ($Name in $Names){
+New-item -type directory -name $Name.Name.Replace(' ','').ToLower() -Path C:\fileserver
+New-item -type file -name hello.txt -Value "Welcome, $($Name.Name)" -Path C:\fileserver\$($Name.Name.Replace(' ','').ToLower())
+}
